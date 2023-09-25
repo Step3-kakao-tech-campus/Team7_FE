1d518ca3ae64c29cddaad25fd2a514d1e4405ad2
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-25 20:22:18 +0900

    chore: MSW 모킹 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,20 @@ import { ThemeProvider } from '@emotion/react';
 import { emotionTheme } from '@/styles/emotion';
 import '@/styles/globals.css';
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  if (typeof window === 'undefined') {
+    (async () => {
+      const { server } = await import('../mocks/server');
+      server.listen();
+    })();
+  } else {
+    (async () => {
+      const { worker } = await import('../mocks/browser');
+      worker.start();
+    })();
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={emotionTheme}>
