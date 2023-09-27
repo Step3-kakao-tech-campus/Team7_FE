2a19eee9360d4fbd8294da515727a1e734874d25
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-27 16:23:35 +0900

    chore: 스토리북 svg 세팅

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,3 @@
-import { css } from '@emotion/react';
-import Button from '@/components/Button';
-import Spinner from '@/components/Spinner';
-
 export default function Home() {
-  return (
-    <div>
-      <Button px={0.6}>
-        <Spinner
-          css={css`
-            margin-right: 0.5rem;
-          `}
-          width={14}
-          height={14}
-        />
-        <Button.Text
-          css={css`
-            margin-right: 0.5rem;
-          `}>
-          Please Wait
-        </Button.Text>
-      </Button>
-    </div>
-  );
+  return <div>홈</div>;
 }
