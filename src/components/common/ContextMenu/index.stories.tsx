5af7f44ae5e475eb191923678ab00979d624d33b
Junseo <monstazo911@gmail.com>
2023-09-28 00:08:53 +0900

    feat: ContextMenu 컴포넌트 구현 및 스토리 작성
    ContextMenu에서 메뉴 개수의 유동성을 고려하여 Composition 방식으로 구현했습니다.
    스토리에는 메뉴가 1개, 2개, 3개인 경우를 작성했습니다.

--- src/components/common/ContextMenu/index.stories.tsx
+++ src/components/common/ContextMenu/index.stories.tsx
@@ -6,4 +6,35 @@ export default {
   title: 'ContextMenu',
 } as Meta<typeof ContextMenu>;
 
-export const Default = {};
+export const Default = {
+  args: {
+    children: (
+      <>
+        <ContextMenu.Menu>수정하기</ContextMenu.Menu>
+        <ContextMenu.Menu>삭제하기</ContextMenu.Menu>
+      </>
+    ),
+  },
+};
+
+export const OneOption = {
+  args: {
+    children: (
+      <>
+        <ContextMenu.Menu>삭제하기</ContextMenu.Menu>
+      </>
+    ),
+  },
+};
+
+export const ThreeOption = {
+  args: {
+    children: (
+      <>
+        <ContextMenu.Menu>정보보기</ContextMenu.Menu>
+        <ContextMenu.Menu>수정하기</ContextMenu.Menu>
+        <ContextMenu.Menu>삭제하기</ContextMenu.Menu>
+      </>
+    ),
+  },
+};
--- src/components/common/ContextMenu/index.tsx
+++ src/components/common/ContextMenu/index.tsx
@@ -1,5 +1,19 @@
-const ContextMenu = (props: Props) => {
-  return <div></div>;
+import type { PropsWithChildren } from 'react';
+import * as Styled from './style';
+
+const ContextMenu = (props: PropsWithChildren) => {
+  const { children } = props;
+  return <Styled.Root>{children}</Styled.Root>;
+};
+
+interface MenuProps {
+  onClick?: () => void;
+}
+
+ContextMenu.Menu = function Menu(props: PropsWithChildren<MenuProps>) {
+  const { children, onClick } = props;
+
+  return <Styled.Menu onClick={onClick}>{children}</Styled.Menu>;
 };
 
 export default ContextMenu;
