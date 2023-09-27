86795b65e5d98915283aac21c61077b2ba83440c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-28 02:18:52 +0900

    feat: Checkbox

---
+++ src/components/common/Checkbox/index.tsx
@@ -0,0 +1,30 @@
+import type { InputHTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+import Flex from '@/components/common/Flex';
+import * as Styled from './style';
+
+export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  checked?: boolean;
+  label?: string;
+  textSize?: number;
+}
+
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
+  const { checked = true, label, textSize = 0.75, ...rest } = props;
+
+  return (
+    <Styled.Label>
+      <Flex>
+        <input ref={ref} type="checkbox" {...rest} />
+        <Styled.Checkbox textSize={textSize} checked={checked}>
+          {checked && <Styled.CheckboxImage src={`/assets/icons/ic_checkIcon.svg`} fill alt="" />}
+        </Styled.Checkbox>
+        <Styled.CheckboxText textSize={textSize} align="center">
+          {label}
+        </Styled.CheckboxText>
+      </Flex>
+    </Styled.Label>
+  );
+});
+
+export default Checkbox;
