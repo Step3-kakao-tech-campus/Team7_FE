2f6ee78ffa1835bae6dcfa90351f8858ed0e31cf
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-30 02:08:18 +0900

    feat: tab 으로 focus 될떄 검은색 적용

--- src/components/common/Modal/style.ts
+++ src/components/common/Modal/style.ts
@@ -24,6 +24,10 @@ export const Container = styled.div<{ width?: number }>`
     0px 4px 6px -4px rgba(0, 0, 0, 0.1),
     0px 10px 15px -3px rgba(0, 0, 0, 0.1),
     0px 0px 0px 1px rgba(0, 0, 0, 0.1);
+
+  & :focus-visible {
+    outline: 2px solid ${({ theme }) => theme.colors.black};
+  }
 `;
 
 export const CloseButton = styled.button`
