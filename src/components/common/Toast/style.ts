140665d50df0f7e36b082699ad85d46ed7bba0c6
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-09-29 21:50:16 +0900

    feat: Toast 컴포넌트

---
+++ src/components/common/Toast/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Root = styled.div`
+  width: 50%;
+  padding: 1.25rem;
+  border-radius: 6px;
+  border: 1px solid #e4e4e7;
+  background-color: #09090b;
+  color: #fff;
+  box-shadow:
+    0px 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0px 4px 6px -4px rgba(16, 24, 40, 0.1);
+`;
