b2593ba0e38e6b32156cb8493fcbd6d04a20d7d0
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-05 01:29:50 +0900

    feat: url에 queryParam을 추가하는 함수

--- src/hooks/common/useParamsToUrl.ts
+++ src/hooks/common/useParamsToUrl.ts
@@ -5,13 +5,10 @@ import type { NextRouter } from 'next/router';
 export const useParamsToUrl = () => {
   const router = useRouter();
 
-  const getParamsToUrl = (queryParams: NextRouter['query']) => {
-    // 현재 url의 query와 새로운 query를 합친다.
-    const mergedParams = { ...router.query, ...queryParams };
-
+  const addParamsToUrl = (queryParams: NextRouter['query']) => {
     router.push(
       // qs.stringify: 객체를 쿼리스트링으로 변환
-      `${router.pathname}${qs.stringify(mergedParams, {
+      `${router.pathname}${qs.stringify(queryParams, {
         addQueryPrefix: true, // prefix로 ?를 붙여준다.
       })}`,
       undefined, // 두번째 옵션은 사용하지 않는다.
@@ -20,6 +17,5 @@ export const useParamsToUrl = () => {
       },
     );
   };
-
-  return { getParamsToUrl };
+  return { addParamsToUrl };
 };
