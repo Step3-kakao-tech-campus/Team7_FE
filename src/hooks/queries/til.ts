f268e85e995d024904df85c959ce97389a61dac5
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-04 13:30:42 +0900

    feat: useGetTils 훅

---
+++ src/hooks/queries/til.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+import { getTils } from '@/api/til';
+import type { TilRequest } from '@/api/til/type';
+
+const QUERY_KEY = {
+  getTils: 'getTils',
+};
+
+export const useGetTils = ({ roadmapId, page, date, title }: TilRequest) => {
+  const { data, isLoading } = useQuery([QUERY_KEY.getTils], () => getTils({ roadmapId, page, date, title }));
+
+  return {
+    // return type에 undefined 제거 하기위해 null 병합 연산자 추가
+    tils: data?.result.tils ?? [],
+    isLoading,
+  };
+};
