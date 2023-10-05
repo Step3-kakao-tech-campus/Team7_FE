36fe85b87680f4de55cdec9e24c102a57141d361
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-06 00:49:51 +0900

    feat: react-resizable-panels 적용

--- src/pages/til-write/index.tsx
+++ src/pages/til-write/index.tsx
@@ -1,9 +1,27 @@
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 import dynamic from 'next/dynamic';
+import styled from '@emotion/styled';
+import Footer from '@/components/TILWrite/Footer';
+import Header from '@/components/TILWrite/Header';
 
 const Editor = dynamic(() => import('@/components/TILWrite/Editor'), { ssr: false });
 
 const TilWrite = () => {
-  return <Editor />;
+  return (
+    <>
+      <Header />
+      <PanelGroup style={{ height: 'calc(100% - 8rem)' }} direction="horizontal">
+        <Panel style={{ overflow: 'scroll', height: 'calc(100% - 30px)' }} defaultSize={70}>
+          <Editor />
+        </Panel>
+        <ResizeHandle />
+        <Panel defaultSize={30} maxSize={30} minSize={0}>
+          <div>dada</div>
+        </Panel>
+      </PanelGroup>
+      <Footer />
+    </>
+  );
 };
 
 export default TilWrite;
