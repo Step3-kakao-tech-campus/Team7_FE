86f4e1b63dbba1e010ea06936005580f823f39a4
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-10 02:20:01 +0900

    feat: 틸 작성페이지 로드맵 section StepList

---
+++ src/components/TILWrite/StepList/index.tsx
@@ -0,0 +1,14 @@
+import Step from '@/components/TILWrite/Step';
+import * as Styled from './style';
+
+const StepList = () => {
+  return (
+    <Styled.Root>
+      {Array.from({ length: 30 }).map((_, index) => {
+        return <Step key={index} />;
+      })}
+    </Styled.Root>
+  );
+};
+
+export default StepList;
---
+++ src/components/TILWrite/StepList/style.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const Root = styled.div`
+  padding: 0.625rem 1rem 0 0;
+`;
