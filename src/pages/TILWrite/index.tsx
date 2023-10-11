ea341c00a869db4bec53d7fc3bc5225beba88d3d
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-11 16:51:26 +0900

    feat: 틸 작성페이지

--- src/pages/TILWrite/index.tsx
+++ src/pages/TILWrite/index.tsx
@@ -1,34 +1,185 @@
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import Comment from '@/components/TILWrite/Comments';
 import Footer from '@/components/TILWrite/Footer';
 import Header from '@/components/TILWrite/Header';
+import Reference from '@/components/TILWrite/Reference';
 import RoadMap from '@/components/TILWrite/RoadMap';
+import type { EmotionTheme } from '@/styles/emotion';
+import { emotionTheme } from '@/styles/emotion';
 
 const Editor = dynamic(() => import('@/components/TILWrite/Ckeditor'), { ssr: false });
 
+const editorVariants = {
+  asideOpen: { width: `${emotionTheme.layout.defaultEditorWidth}` },
+  asideClosed: { width: `calc(${emotionTheme.layout.maxEditorWidth} - ${emotionTheme.layout.resizeHandleWidth} )` },
+};
+
+// const asideVariants = {
+//   open: { opacity: 1, x: 0 },
+//   closed: { opacity: 1, x: 'calc(100% - 60px)' },
+// };
+
+const extraDrawerVariants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 1, x: '100%' },
+};
+
 const TILWrite = () => {
+  const [referenceOpen, setReferenceOpen] = useState(false);
+  const [referenceMount, setReferenceMount] = useState(false);
+  const [commentOpen, setCommentOpen] = useState(false);
+  const [commentMount, setCommentMount] = useState(false);
+  const [asideOpen, setAsideOpen] = useState(true);
+  const [asideMount, setAsideMount] = useState(true);
+  const handleCloseAside = () => {
+    setAsideOpen(false);
+    setReferenceOpen(false);
+  };
+
+  const handleCloseReferenceAside = () => {
+    setReferenceOpen(false);
+  };
+
+  const handleOpenReferenceAside = () => {
+    setReferenceOpen(true);
+  };
+
+  const handleOpenCommentAside = () => {
+    setCommentOpen(true);
+  };
+
+  const handleCloseCommentAside = () => {
+    setCommentOpen(false);
+  };
+
+  useEffect(() => {
+    // aside가 마운트될때 drawer가 열리면 레이아웃 깨지는 현상이 발생하여 딜레이를 주었다.
+    if (asideOpen) {
+      setTimeout(() => {
+        setAsideMount(true);
+      }, 300);
+    } else {
+      setAsideMount(false);
+    }
+  }, [asideOpen]);
+
+  useEffect(() => {
+    // aside가 마운트될때 drawer가 열리면 레이아웃 깨지는 현상이 발생하여 딜레이를 주었다.
+    if (referenceOpen) {
+      setTimeout(() => {
+        setReferenceMount(true);
+      }, 300);
+    } else {
+      setReferenceMount(false);
+    }
+  }, [referenceOpen]);
+
+  useEffect(() => {
+    // aside가 마운트될때 drawer가 열리면 레이아웃 깨지는 현상이 발생하여 딜레이를 주었다.
+    if (commentOpen) {
+      setAsideOpen(true);
+      setTimeout(() => {
+        setCommentMount(true);
+      }, 300);
+    } else {
+      setReferenceOpen(false);
+      setCommentMount(false);
+    }
+  }, [commentOpen]);
+
   return (
-    <>
-      <Header />
-      <PanelGroup style={{ height: 'calc(100% - 8rem)' }} direction="horizontal">
-        <Panel style={{ overflowY: 'scroll', height: 'calc(100% - 30px)' }} defaultSize={70}>
+    <Root>
+      <Header handleOpenCommentAside={handleOpenCommentAside} />
+      <Container>
+        <motion.div
+          css={EditorContainerStyles}
+          initial="asideOpen"
+          animate={asideOpen ? 'asideOpen' : 'asideClosed'}
+          variants={editorVariants}
+          transition={{ type: 'tween', duration: 0.3 }}>
           <Editor />
-        </Panel>
-        <ResizeHandle />
-        <Panel style={{ overflowY: 'scroll' }} defaultSize={30} maxSize={30} minSize={0}>
-          <RoadMap />
-        </Panel>
-      </PanelGroup>
+        </motion.div>
+
+        <motion.aside css={AsideStyles}>
+          <ResizeHandle
+            onClick={() => {
+              console.log('click');
+              setReferenceOpen(false);
+              setCommentOpen(false);
+              setAsideOpen((prev) => !prev);
+            }}
+          />
+          {asideMount && asideOpen && (
+            <RoadMap handleCloseAside={handleCloseAside} handleOpenReferenceAside={handleOpenReferenceAside} />
+          )}
+
+          <motion.aside
+            css={extraDrawerStyles}
+            initial="closed"
+            animate={referenceOpen ? 'open' : 'closed'}
+            variants={extraDrawerVariants}
+            transition={{ type: 'tween' }}>
+            {referenceMount && referenceOpen && <Reference handleCloseReferenceAside={handleCloseReferenceAside} />}
+          </motion.aside>
+
+          <motion.aside
+            css={extraDrawerStyles}
+            initial="closed"
+            animate={commentOpen ? 'open' : 'closed'}
+            variants={extraDrawerVariants}
+            transition={{ type: 'tween' }}>
+            {commentMount && commentOpen && <Comment handleCloseCommentAside={handleCloseCommentAside} />}
+          </motion.aside>
+        </motion.aside>
+      </Container>
       <Footer />
-    </>
+    </Root>
   );
 };
 
 export default TILWrite;
 
-const ResizeHandle = styled(PanelResizeHandle)`
-  width: 50px;
+const Root = styled.div`
+  overflow-x: hidden;
+  height: 100%;
+`;
+
+const EditorContainerStyles = (theme: EmotionTheme) => css`
+  width: ${theme.layout.defaultEditorWidth};
+  flex-shrink: 0;
+  overflow-y: scroll;
+  background-color: #fff;
+`;
+
+const ResizeHandle = styled.div`
+  position: sticky;
+  top: 0;
+  width: ${({ theme }) => theme.layout.resizeHandleWidth};
   height: 100%;
   background-image: url('/assets/icons/ic_spring.svg');
+  cursor: pointer;
+`;
+const Container = styled.div`
+  display: flex;
+  height: ${({ theme }) => `calc(100% - ${theme.layout.headerHeight} - ${theme.layout.footerHeight})`};
+`;
+
+const AsideStyles = css`
+  display: flex;
+  flex: 1;
+`;
+
+const extraDrawerStyles = (theme: EmotionTheme) => css`
+  position: fixed;
+  top: ${theme.layout.headerHeight};
+  left: calc(${theme.layout.defaultEditorWidth} + ${theme.layout.resizeHandleWidth});
+  z-index: 100;
+  width: calc(${theme.layout.asideWidth});
+  height: calc(100% - ${theme.layout.headerHeight} - ${theme.layout.footerHeight});
+  background-color: white;
+  overflow-y: scroll;
 `;
