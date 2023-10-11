b2c09894fc4b3f5135f0285d143757ed1111a26e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-12 03:05:54 +0900

    refactor: 모달 컴포넌트 style 주입, 이미지 사이즈 수정

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -2,18 +2,32 @@ import FocusTrap from 'focus-trap-react';
 import { type PropsWithChildren, type HTMLAttributes, useRef } from 'react';
 import { RemoveScroll } from 'react-remove-scroll';
 import Image from 'next/image';
+import type { SerializedStyles } from '@emotion/react';
 import Portal from '@/components/common/Portal';
 import { useOnClickOutside } from '@/hooks/common/useOnClickOutside';
+import type { EmotionTheme } from '@/styles/emotion';
 import * as Styled from './style';
 
 export interface ModalProps extends HTMLAttributes<HTMLDivElement> {
   width?: number;
   isOpen?: boolean;
   onClose: () => void;
+  modalContentStyles?: (theme: EmotionTheme) => SerializedStyles;
+  closeButtonStyles?: (theme: EmotionTheme) => SerializedStyles;
+  closeButtonSize?: number;
 }
 
 const Modal = (props: PropsWithChildren<ModalProps>) => {
-  const { children, width, isOpen, onClose, ...rest } = props;
+  const {
+    children,
+    width,
+    isOpen,
+    onClose,
+    modalContentStyles,
+    closeButtonStyles,
+    closeButtonSize = 24,
+    ...rest
+  } = props;
 
   const modalRef = useRef<HTMLDivElement>(null);
 
@@ -31,10 +45,15 @@ const Modal = (props: PropsWithChildren<ModalProps>) => {
         <FocusTrap>
           <RemoveScroll>
             <Styled.Container role="dialog" ref={modalRef} width={width} {...rest}>
-              <Styled.CloseButton onClick={onClose}>
-                <Image src="/assets/icons/ic_closeButton.svg" alt="close" width={24} height={24} />
+              <Styled.CloseButton css={closeButtonStyles} onClick={onClose}>
+                <Image
+                  src="/assets/icons/ic_closeButton.svg"
+                  alt="close"
+                  width={closeButtonSize}
+                  height={closeButtonSize}
+                />
               </Styled.CloseButton>
-              <Styled.Content>{children}</Styled.Content>
+              <Styled.Content css={modalContentStyles}>{children}</Styled.Content>
             </Styled.Container>
           </RemoveScroll>
         </FocusTrap>
