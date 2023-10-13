0432d4b55efb639f8383dd8631c2abafe2efe58c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-13 15:00:52 +0900

    feat: postTil API

--- src/api/til/index.ts
+++ src/api/til/index.ts
@@ -1,10 +1,23 @@
 import { axiosInstance } from '@/api';
-import type { TilsRequest, TilsResponse } from '@/api/til/type';
+import type { TilsRequest, TilsResponse, PostTilRequest, PostTilResponse } from '@/api/til/type';
 
 export const getTils = async (input: TilsRequest) => {
   const { data } = await axiosInstance.request<TilsResponse>({
     method: 'GET',
     url: `/tils/my/${input}`,
+  });
+
+  return data;
+};
+//
+
+export const postTil = async (param: PostTilRequest) => {
+  const { roadmapId, stepId, title } = param;
+
+  const { data } = await axiosInstance.request<PostTilResponse>({
+    method: 'GET',
+    url: `/roadmaps/${roadmapId}/steps/${stepId}/tils`,
+    data: { title },
   });
 
   return data;
