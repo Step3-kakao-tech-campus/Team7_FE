79d77dac0036731c30aa128f1fa4a10706f138db
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-14 18:52:57 +0900

    feat: 알람 UI 데이터 주입

--- src/components/common/GNB/Alarm/index.tsx
+++ src/components/common/GNB/Alarm/index.tsx
@@ -1,22 +1,30 @@
 import Image from 'next/image';
+import { useGetAlarms } from '@/api/hooks/user';
 import Avatar from '@/components/common/Avatar';
 import * as Styled from './style';
 
-const Alarm = () => {
+  const { alarms } = useGetAlarms();
   return (
     <Styled.Root>
       <Styled.Header>알림</Styled.Header>
 
       <Styled.List>
-        <Styled.Item>
-          <Avatar imageSize={40} iconName="ic_profile" />
-          <Styled.Content>
-            <Styled.Title>Next JS 입문 - 6. SPA</Styled.Title>
-            <Styled.Description>
-              <Styled.Commenter>
-                <span>동영</span>
-                <span>님이 댓글을 남겼습니다.</span>
-              </Styled.Commenter>
+        {alarms.length === 0 ? (
+          <EmptyAlarm />
+        ) : (
+          alarms.map((alarm) => {
+            return (
+              <Styled.Item key={alarm.id}>
+                <Avatar imageSize={40} imageUrl={alarm.sender.image} alt="프로필 이미지" />
+                <Styled.Content>
+                  <Styled.Title>
+                    {alarm.roadmap.name} - {alarm.step.name}
+                  </Styled.Title>
+                  <Styled.Description>
+                    <Styled.Commenter>
+                      <span>{alarm.sender.name}</span>
+                      <span>님이 댓글을 남겼습니다.</span>
+                    </Styled.Commenter>
 
               <Styled.Time>2023.06.16</Styled.Time>
             </Styled.Description>
