a85db94dee8c659aa352e46c5f67337f849de9c1
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-14 18:53:46 +0900

    feat: patchAlarm 모킹

--- src/mocks/handlers/user.ts
+++ src/mocks/handlers/user.ts
@@ -1,5 +1,10 @@
 import { rest } from 'msw';
-import { getUserResponse, userHistoryResponse } from '@/mocks/fixtures/user';
+import {
+  getAlarmsResponse,
+  getUserResponse,
+  userHistoryResponse,
+  updateGetAlarmsResponseFixture,
+} from '@/mocks/fixtures/user';
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -22,6 +27,44 @@ export const userHandler = [
   rest.get(`${BASE_URL}/users`, (req, res, ctx) => {
     try {
       return res(ctx.status(200), ctx.json(getUserResponse));
+    } catch (error) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          success: false,
+          message: '서버에서 에러가 났어요',
+          result: null,
+        }),
+      );
+    }
+  }),
+
+  rest.get(`${BASE_URL}/alarms`, (req, res, ctx) => {
+    try {
+      return res(ctx.status(200), ctx.json(getAlarmsResponse));
+    } catch (error) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          success: false,
+          message: '서버에서 에러가 났어요',
+          result: null,
+        }),
+      );
+    }
+  }),
+
+  rest.patch(`${BASE_URL}/alarms/read`, (req, res, ctx) => {
+    try {
+      updateGetAlarmsResponseFixture();
+      return res(
+        ctx.status(200),
+        ctx.json({
+          success: false,
+          message: 'ok',
+          result: null,
+        }),
+      );
     } catch (error) {
       return res(
         ctx.status(400),
