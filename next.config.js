d3dfbad87ceae5837f52fe5bcffc2dcaf9aaceed
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-14 16:34:31 +0900

    feat: 외부 도메인 이미지 사용을 위하여 코드 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
