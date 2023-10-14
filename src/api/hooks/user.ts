627de08188e38e9e27d4bc51d5b6b30f6c05d62c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-14 18:52:02 +0900

    feat: usePatchAlarm

--- src/api/hooks/user.ts
+++ src/api/hooks/user.ts
@@ -1,5 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import { getAlarms, getUser, getUserHistory } from '@/api/user';
+import { patchAlarm as patchAlarmAPI } from '@/api/user';
+import type { PatchAlarmRequest } from '@/api/user/type';
 
 const QUERY_KEY = {
   userHistory: 'userHistory',
@@ -28,7 +31,36 @@ export const useGetUser = () => {
 export const useGetAlarms = () => {
   const { data } = useQuery([QUERY_KEY.alarm], () => getAlarms());
 
+  const isNewAlarm = data?.result.alarms.some((alarm) => alarm.isChecked === false);
+
+  const patchAlarmRequset: PatchAlarmRequest = {
+    alarms:
+      data?.result.alarms.map((alarm) => ({
+        id: alarm.id,
+      })) ?? [],
+  };
+
   return {
     alarms: data?.result.alarms ?? [],
+    isNewAlarm,
+    patchAlarmRequset,
   };
+};
+
+export const usePatchAlarm = () => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(patchAlarmAPI);
+
+  const patchAlarm = async (body: PatchAlarmRequest) => {
+    const data = await mutation.mutateAsync(body, {
+      onSuccess: () => {
+        queryClient.invalidateQueries([QUERY_KEY.alarm]);
+      },
+    });
+
+    return data;
+  };
+
+  return { patchAlarm };
 };
