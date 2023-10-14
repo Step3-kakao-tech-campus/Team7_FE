04ebd0afb2d9db11dc50ed36e886a4be2f230c85
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-14 18:53:14 +0900

    design: 알림 없음

--- src/components/common/GNB/Alarm/style.ts
+++ src/components/common/GNB/Alarm/style.ts
@@ -106,8 +106,11 @@ export const EmptyAlarmRoot = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 3rem;
 `;
 
 export const EmptyAlarmText = styled.p`
+  margin-top: 1rem;
   font-weight: 600;
+  font-size: 1.125rem;
 `;
