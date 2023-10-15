601062751a296597b6c8921fd8cde30eaec2d88e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-15 16:05:49 +0900

    refactor: getTilWriteUrl 제거

--- src/components/main/TIL/index.tsx
+++ src/components/main/TIL/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
-import type { Til } from '@/api/til/type';
-import { getTilWriteUrl } from '@/utils/getTilWriteUrl';
+import type { Til } from '@/api/type';
+import { tilyLinks } from '@/constants/links';
 import * as Styled from './style';
 
 interface TILProps {
@@ -13,7 +13,13 @@ const TIL = (props: TILProps) => {
   const router = useRouter();
 
   return (
-    <Styled.Root onClick={() => router.push(getTilWriteUrl(til.roadmap.id, til.step.id, til.id))}>
+    <Styled.Root
+      onClick={() =>
+        router.push({
+          pathname: tilyLinks.tilWrite(),
+          query: { roadmapId: til.roadmap.id, stepId: til.step.id, tilId: til.id },
+        })
+      }>
       <Styled.Badge>{til.roadmap.name}</Styled.Badge>
       <Styled.Title>{til.step.title}</Styled.Title>
       <Styled.Date>{til.createDate}</Styled.Date>
--- src/utils/getTilWriteUrl.ts
+++
@@ -1,3 +0,0 @@
-export const getTilWriteUrl = (roadmapId: number, stepId: number, tilId: number) => {
-  return `/TILWrite?roadmapId=${roadmapId}&?stepId=${stepId}&?tilId=${tilId}`;
-};
