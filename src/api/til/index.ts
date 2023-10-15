3c4ecaa46b8e51f78ca12612f6aefe064326e5e0
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-15 15:42:58 +0900

    feat: getTil API

--- src/api/til/index.ts
+++ src/api/til/index.ts
@@ -1,5 +1,12 @@
 import { axiosInstance } from '@/api';
-import type { TilsRequest, TilsResponse, PostTilRequest, PostTilResponse } from '@/api/til/type';
+import type {
+  TilsRequest,
+  TilsResponse,
+  PostTilRequest,
+  PostTilResponse,
+  GetTilRequest,
+  GetTilResponse,
+} from '@/api/til/type';
 
 export const getTils = async (input: TilsRequest) => {
   const { data } = await axiosInstance.request<TilsResponse>({
@@ -9,7 +16,6 @@ export const getTils = async (input: TilsRequest) => {
 
   return data;
 };
-//
 
 export const postTil = async (body: PostTilRequest) => {
   const { roadmapId, stepId, title } = body;
@@ -18,6 +24,17 @@ export const postTil = async (body: PostTilRequest) => {
     method: 'POST',
     url: `/roadmaps/${roadmapId}/steps/${stepId}/tils`,
     data: { title },
+  });
+
+  return data;
+};
+
+export const getTil = async (body: GetTilRequest) => {
+  const { roadmapId, stepId, tilId } = body;
+
+  const { data } = await axiosInstance.request<GetTilResponse>({
+    method: 'GET',
+    url: `/roadmaps/${roadmapId}/steps/${stepId}/tils/${tilId}`,
   });
 
   return data;
