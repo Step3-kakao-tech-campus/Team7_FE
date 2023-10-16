a60c8a8ad201a5792da5f018a90410def4fef623
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-17 08:06:33 +0900

    feat: 카드 컴포넌트

---
+++ src/components/roadmap/PeopleTIL/FeatureInfoSection/index.tsx
@@ -0,0 +1,13 @@
+import Card from '@/components/common/Card';
+import * as Styled from './style';
+
+const FeatureInfoSection = () => {
+  return (
+    <Styled.Root>
+      <Card css={Styled.CardStyles}>공부 방향에 대한 고민이 있는 분들께 드리는 TIL-y의 로드맵!</Card>
+      <Card css={Styled.CardStyles}>공부 방향에 대한 고민이 있는 분들께 드리는 TIL-y의 로드맵!</Card>
+    </Styled.Root>
+  );
+};
+
+export default FeatureInfoSection;
---
+++ src/components/roadmap/PeopleTIL/FeatureInfoSection/style.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Root = styled.div`
+  display: flex;
+  gap: 2rem;
+  width: 100%;
+`;
+
+export const CardStyles = css`
+  flex: 1;
+  font-size: 1.5rem;
+  font-weight: 700;
+  height: 180px;
+`;
