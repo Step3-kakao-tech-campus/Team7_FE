ff3c583d41bda3631b25d7db8b04c5d004ac1886
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-19 17:49:29 +0900

    feat: ConditionalRender 에 undefined 방어코드 적용

--- src/components/common/ConditionalRender/index.tsx
+++ src/components/common/ConditionalRender/index.tsx
@@ -1,12 +1,16 @@
 import type { PropsWithChildren } from 'react';
 
 interface ConditionalRenderProps {
-  data: Array<any>;
+  data?: Array<any>;
   EmptyUI: React.ReactNode;
 }
 
 const ConditionalRender = (props: PropsWithChildren<ConditionalRenderProps>) => {
   const { data, EmptyUI, children } = props;
+
+  if (!data) {
+    return null;
+  }
 
   return <>{data.length === 0 ? EmptyUI : children}</>;
 };
