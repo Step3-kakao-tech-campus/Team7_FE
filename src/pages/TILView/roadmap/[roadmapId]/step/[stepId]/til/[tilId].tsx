9304edf7e228ab218c78d141979c080584fe5d95
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-20 03:17:24 +0900

    refactor: 불필요한 코드 제거

--- src/pages/TILView/roadmap/[roadmapId]/step/[stepId]/til/[tilId].tsx
+++ src/pages/TILView/roadmap/[roadmapId]/step/[stepId]/til/[tilId].tsx
@@ -1,36 +1,23 @@
-import { motion } from 'framer-motion';
-import { useState } from 'react';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
-import styled from '@emotion/styled';
-import { useGetTil } from '@/api/hooks/til';
 import Comment from '@/components/TILView/Comments';
 import Footer from '@/components/TILView/Footer';
 import Header from '@/components/TILView/Header';
-import Reference from '@/components/TILView/Reference';
-import RoadMap from '@/components/TILView/RoadMap';
 import EmptyLayout from '@/components/layout/EmptyLayout';
 import { useDrawerState } from '@/hooks/useDrawerState';
-import { emotionTheme } from '@/styles/emotion';
+import {
+  editorVariants,
+  DURATION,
+  Root,
+  EditorContainer,
+  ResizeHandle,
+  Container,
+  AsideContainer,
+} from '@/pages/TILWrite/roadmap/[roadmapId]/step/[stepId]/til/[tilId]';
 import { setLayout } from '@/utils/layout';
 
 const Editor = dynamic(() => import('@/components/TILView/Ckeditor'), { ssr: false });
 
 const TILView = () => {
-  const [TILContent, setTILContent] = useState<string>('');
-
-  const { query } = useRouter();
-
-  const { tilDetail } = useGetTil({
-    roadmapId: Number(query.roadmapId),
-    stepId: Number(query.stepId),
-    tilId: Number(query.tilId),
-  });
-
-  const handleTILContent = (content: string) => {
-    setTILContent(content);
-  };
-
   const {
     isOpen: asideOpen,
     isMount: asideMount,
@@ -42,83 +29,29 @@ const TILView = () => {
     duration: DURATION,
   });
 
-  const {
-    isOpen: referenceOpen,
-    handleClose: handleCloseReference,
-    handleOpen: handleOpenReference,
-  } = useDrawerState({
-    defaultValue: false,
-    duration: DURATION,
-  });
-
-  const {
-    isOpen: commentOpen,
-    handleClose: handleCloseComment,
-    handleOpen: handleOpenComment,
-  } = useDrawerState({
-    defaultValue: false,
-    duration: DURATION,
-    runWhenTrue: () => {
-      handleOpenAside();
-      handleCloseReference();
-    },
-  });
-
   const handleToggleResize = () => {
-    handleCloseReference();
-    handleCloseComment();
     handleToggleAside();
   };
 
   return (
     <Root>
-      <Header handleOpenCommentAside={handleOpenComment} />
+      <Header handleOpenCommentAside={handleOpenAside} />
       <Container>
-        {tilDetail?.isPersonal === false ? (
-          <EditorContainer
-            initial="asideOpen"
-            animate={asideOpen ? 'asideOpen' : 'asideClosed'}
-            variants={editorVariants}
-            transition={{ type: 'tween', duration: DURATION }}>
-            <Editor handleTILContent={handleTILContent} />
-          </EditorContainer>
-        ) : (
-          <PersonalEditorContainer>
-            <Editor handleTILContent={handleTILContent} />
-          </PersonalEditorContainer>
-        )}
+        <EditorContainer
+          initial="asideOpen"
+          animate={asideOpen ? 'asideOpen' : 'asideClosed'}
+          variants={editorVariants}
+          transition={{ type: 'tween', duration: DURATION }}>
+          <Editor />
+        </EditorContainer>
 
-        {tilDetail?.isPersonal === false && (
-          <AsideContainer>
-            <ResizeHandle onClick={handleToggleResize} />
+        <AsideContainer>
+          <ResizeHandle onClick={handleToggleResize} />
 
-            {asideOpen && (
-              <RoadMap
-                asideMount={asideMount}
-                handleCloseAside={() => handleCloseAside(handleCloseReference)}
-                handleOpenReferenceAside={handleOpenReference}
-              />
-            )}
-
-            <ExtraDrawerMotion
-              initial="closed"
-              animate={referenceOpen ? 'open' : 'closed'}
-              variants={extraDrawerVariants}
-              transition={{ type: 'tween', duration: DURATION }}>
-              <Reference handleCloseReferenceAside={() => handleCloseReference()} />
-            </ExtraDrawerMotion>
-
-            <ExtraDrawerMotion
-              initial="closed"
-              animate={commentOpen ? 'open' : 'closed'}
-              variants={extraDrawerVariants}
-              transition={{ type: 'tween', duration: DURATION }}>
-              <Comment handleCloseCommentAside={() => handleCloseComment()} />
-            </ExtraDrawerMotion>
-          </AsideContainer>
-        )}
+          {asideOpen && <Comment asideMount={asideMount} handleCloseCommentAside={() => handleCloseAside()} />}
+        </AsideContainer>
       </Container>
-      <Footer TILContent={TILContent} />
+      <Footer />
     </Root>
   );
 };
@@ -126,71 +59,3 @@ const TILView = () => {
 export default TILView;
 
 setLayout(TILView, EmptyLayout, true);
-
-const editorVariants = {
-  asideOpen: {
-    width: `${emotionTheme.layout.tilWrite.defaultEditorWidth}`,
-  },
-  asideClosed: {
-    width: `calc(${emotionTheme.layout.tilWrite.maxEditorWidth} - ${emotionTheme.layout.tilWrite.resizeHandleWidth} )`,
-  },
-};
-
-const extraDrawerVariants = {
-  open: { x: 0, opacity: 1 },
-  closed: { x: '100%', opacity: 0 },
-};
-
-const DURATION = 0.3;
-
-const Root = styled.div`
-  overflow-x: hidden;
-  height: 100%;
-`;
-
-const EditorContainer = styled(motion.div)`
-  width: ${({ theme }) => theme.layout.tilWrite.defaultEditorWidth};
-  flex-shrink: 0;
-  overflow-y: scroll;
-  background-color: #fff;
-`;
-
-const PersonalEditorContainer = styled.div`
-  width: ${({ theme }) => theme.layout.tilWrite.maxEditorWidth};
-  flex-shrink: 0;
-  overflow-y: scroll;
-  background-color: #fff;
-`;
-
-const ResizeHandle = styled.div`
-  position: sticky;
-  top: 0;
-  width: ${({ theme }) => theme.layout.tilWrite.resizeHandleWidth};
-  height: 100%;
-  background-image: url('/assets/icons/ic_spring.svg');
-  cursor: pointer;
-`;
-
-const Container = styled.div`
-  display: flex;
-  height: ${({ theme }) =>
-    `calc(100% - ${theme.layout.tilWrite.headerHeight} - ${theme.layout.tilWrite.footerHeight})`};
-`;
-
-const AsideContainer = styled.aside`
-  display: flex;
-  flex: 1;
-`;
-
-const ExtraDrawerMotion = styled(motion.div)`
-  position: fixed;
-  top: ${({ theme }) => theme.layout.tilWrite.headerHeight};
-  left: ${({ theme }) =>
-    `calc(${theme.layout.tilWrite.defaultEditorWidth} + ${theme.layout.tilWrite.resizeHandleWidth})`};
-  z-index: 200;
-  width: ${({ theme }) => `calc(${theme.layout.tilWrite.asideWidth})`};
-  height: ${({ theme }) =>
-    `calc(100% - ${theme.layout.tilWrite.headerHeight} - ${theme.layout.tilWrite.footerHeight})`};
-  background-color: white;
-  overflow-y: scroll;
-`;
