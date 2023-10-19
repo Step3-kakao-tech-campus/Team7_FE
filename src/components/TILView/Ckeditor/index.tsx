07b62b5ed01fec4510e2d44b0c800b856587fa88
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-20 03:16:27 +0900

    feat: TILView 에디터 readOnly 모드 추가

--- src/components/TILView/Ckeditor/index.tsx
+++ src/components/TILView/Ckeditor/index.tsx
@@ -2,17 +2,9 @@ import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import { useRouter } from 'next/router';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { useGetTil } from '@/api/hooks/til';
-import { defaultData } from '@/components/TILWrite/Ckeditor/defaultData';
 import * as Styled from '@/components/TILWrite/Ckeditor/style';
-import { editorConfiguration } from './plugin';
 
-interface CkEditorProps {
-  handleTILContent: (content: string) => void;
-}
-
-const CkEditor = (props: CkEditorProps) => {
-  const { handleTILContent } = props;
-
+const CkEditor = () => {
   const { query } = useRouter();
 
   const { tilDetail } = useGetTil({
@@ -22,25 +14,12 @@ const CkEditor = (props: CkEditorProps) => {
   });
 
   return (
-    <Styled.Root>
+    <Styled.Root isReadOnly={true}>
       <CKEditor
         editor={Editor}
-        config={editorConfiguration}
-        data={tilDetail ? tilDetail.content : defaultData}
+        data={tilDetail?.content}
         onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          handleTILContent(editor.getData());
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          console.log({ event, editor, data });
-          handleTILContent(editor.getData());
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
+          editor.enableReadOnlyMode('');
         }}
       />
     </Styled.Root>
