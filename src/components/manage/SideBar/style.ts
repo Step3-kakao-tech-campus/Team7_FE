fd6faeca4d69ad7941a5c1badb25af703507e78e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-20 05:58:26 +0900

    feat: 사이드바 컴포넌트 추가

---
+++ src/components/manage/SideBar/style.ts
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 6rem;
+  gap: 4px;
+`;
+
+export const NavItem = styled(Link)<{ active?: number }>`
+  font-size: 1.125rem;
+  font-weight: 700;
+  padding: 0.7rem;
+  border-radius: 8px;
+
+  ${({ active, theme }) => css`
+    background-color: ${active ? theme.colors.rose_light : 'transparent'};
+  `}
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray_200};
+  }
+`;
