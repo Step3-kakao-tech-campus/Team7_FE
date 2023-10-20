b05dd5df8258bbed3dd5e1d01771a8e189ac1ae1
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-20 22:20:10 +0900

    feat: 403 에러 발생시 에러 처리

--- src/components/common/GlobalErrorBoundary/index.tsx
+++ src/components/common/GlobalErrorBoundary/index.tsx
@@ -13,12 +13,29 @@ export interface ErrorBoundaryProps {
 const GlobalErrorBoundary = (props: ErrorBoundaryProps) => {
   const { error, resetErrorBoundary } = props;
 
+  if (error.response?.status === 403) {
+    return <Fallback desc={'접근 권한이 없습니다'} resetErrorBoundary={resetErrorBoundary} />;
+  }
+
+  return <Fallback desc={'오류가 발생했습니다'} resetErrorBoundary={resetErrorBoundary} />;
+};
+
+export default GlobalErrorBoundary;
+
+interface FallbackProps {
+  desc: string;
+  resetErrorBoundary: () => void;
+}
+
+const Fallback = (props: FallbackProps) => {
+  const { desc, resetErrorBoundary } = props;
+
   const router = useRouter();
 
   return (
     <Styled.Root>
       <Image src="/assets/icons/500.svg" width={300} height={300} alt="페이지 에러" />
-      <h1>오류가 발생했습니다</h1>
+      <h1>{desc}</h1>
       <Button
         variant="default"
         onClick={() => {
@@ -33,5 +50,3 @@ const GlobalErrorBoundary = (props: ErrorBoundaryProps) => {
     </Styled.Root>
   );
 };
-
-export default GlobalErrorBoundary;
