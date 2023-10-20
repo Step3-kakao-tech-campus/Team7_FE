a3886719710e50189cc3932bcb653c85132aa906
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-20 22:23:30 +0900

    feat: 라우트 정보에 따라 사이드바 액티브 표시

--- src/components/manage/SideBar/index.tsx
+++ src/components/manage/SideBar/index.tsx
@@ -7,20 +7,28 @@ import * as Styled from './style';
 const SideBar = () => {
   const router = useRouter();
 
-  const activePathMatcher = (path: string) => router.pathname === path;
+  const activePathMatcher = (path: string) => router.pathname.split('/')[4] === path.split('/')[4];
 
   return (
     <Styled.Root>
-      <Styled.NavItem href={tilyLinks.manageGroupInfo()} active={+activePathMatcher(tilyLinks.manageGroupInfo())}>
+      <Styled.NavItem
+        href={tilyLinks.manageGroupInfo(Number(router.query.roadmapId))}
+        active={+activePathMatcher(tilyLinks.manageGroupInfo(Number(router.query.roadmapId)))}>
         그룹 정보
       </Styled.NavItem>
-      <Styled.NavItem href={tilyLinks.manageMember()} active={+activePathMatcher(tilyLinks.manageMember())}>
+      <Styled.NavItem
+        href={tilyLinks.manageMember(Number(router.query.roadmapId))}
+        active={+activePathMatcher(tilyLinks.manageMember(Number(router.query.roadmapId)))}>
         구성원 관리
       </Styled.NavItem>
-      <Styled.NavItem href={tilyLinks.manageTIL()} active={+activePathMatcher(tilyLinks.manageTIL())}>
+      <Styled.NavItem
+        href={tilyLinks.manageTIL(Number(router.query.roadmapId))}
+        active={+activePathMatcher(tilyLinks.manageTIL(Number(router.query.roadmapId)))}>
         TIL 모아보기
       </Styled.NavItem>
-      <Styled.NavItem href={tilyLinks.manageApply()} active={+activePathMatcher(tilyLinks.manageApply())}>
+      <Styled.NavItem
+        href={tilyLinks.manageApply(Number(router.query.roadmapId))}
+        active={+activePathMatcher(tilyLinks.manageApply(Number(router.query.roadmapId)))}>
         신청 관리
       </Styled.NavItem>
     </Styled.Root>
