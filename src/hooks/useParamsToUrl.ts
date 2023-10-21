d7a5d0b21c6e60453752aa62976795bff2754297
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-22 01:59:06 +0900

    feat: 동적 라우팅 실제 query 데이터로 매핑하는 replaceDynamicPath

--- src/hooks/useParamsToUrl.ts
+++ src/hooks/useParamsToUrl.ts
@@ -5,10 +5,14 @@ import type { NextRouter } from 'next/router';
 export const useParamsToUrl = () => {
   const router = useRouter();
 
+  const replaceDynamicPath = (path: string, query: any) => {
+    return path.replace(/\[(\w+)\]/g, (match, key) => query[key] || match);
+  };
+
   const addParamsToUrl = (queryParams: NextRouter['query']) => {
     router.push(
       // qs.stringify: 객체를 쿼리스트링으로 변환
-      `${router.pathname}${qs.stringify(queryParams, {
+      `${replaceDynamicPath(router.pathname, router.query)}${qs.stringify(queryParams, {
         addQueryPrefix: true, // prefix로 ?를 붙여준다.
       })}`,
       undefined, // 두번째 옵션은 사용하지 않는다.
