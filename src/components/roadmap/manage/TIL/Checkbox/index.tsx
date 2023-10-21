14b01d5cf2aa09bd10440aa799d1f8008813e132
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-22 05:11:56 +0900

    feat: 체크박스 컴포넌트

---
+++ src/components/roadmap/manage/TIL/Checkbox/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import CommonCheckbox from '@/components/common/Checkbox';
+import { useParamsToUrl } from '@/hooks/useParamsToUrl';
+import * as Styled from './style';
+
+const Checkbox = () => {
+  const [checked, setChecked] = useState<boolean>(MEMBER);
+
+  const router = useRouter();
+  const { overlapParamsToUrl } = useParamsToUrl();
+
+  useEffect(() => {
+    setChecked(router.query.isMember === 'false' ? MASTER_MANAGER : MEMBER);
+  }, [router.isReady]);
+
+  return (
+    <Styled.Root>
+      <CommonCheckbox
+        textSize={0.875}
+        label="검색 결과에 마스터 · 매니저 포함"
+        checked={checked}
+        onChange={(e) => {
+          setChecked(e.target.checked);
+          overlapParamsToUrl({ isMember: (!e.target.checked).toString() });
+        }}
+      />
+    </Styled.Root>
+  );
+};
+
+export default Checkbox;
+
+const MASTER_MANAGER = true;
+const MEMBER = false;
