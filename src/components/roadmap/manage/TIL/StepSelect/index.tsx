e95fb1082d09fdedcbfe42cf1ca7c512a8eabdc2
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-22 02:00:31 +0900

    feat: Step Select

---
+++ src/components/roadmap/manage/TIL/StepSelect/index.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useGetRoadmapSteps } from '@/api/hooks/roadmap';
+import Select from '@/components/common/Select';
+import type { SelectOption } from '@/components/common/Select';
+import { useParamsToUrl } from '@/hooks/useParamsToUrl';
+import * as Styled from './style';
+
+const StepSelect = () => {
+  const [selectedOption, setSelectedOption] = useState<SelectOption | undefined>(defaultValue);
+  const [selectOptionItems, setSelectOptionItems] = useState<SelectOption[]>([]);
+
+  const router = useRouter();
+
+  const { steps } = useGetRoadmapSteps(Number(router.query.roadmapId));
+  const { addParamsToUrl } = useParamsToUrl();
+
+  useEffect(() => {
+    // 초기 useEffect에서 steps가 undefined일 경우 return
+    if (!steps || !router.isReady) return;
+
+    const transformData = steps.result.steps.map((step) => {
+      return {
+        label: step.title,
+        value: step.id.toString(),
+      };
+    });
+
+    addParamsToUrl({ stepId: router.query.stepId ? router.query.stepId : transformData[0].value });
+    setSelectedOption(
+      router.query.stepId ? transformData.find((data) => data.value === router.query.stepId) : transformData[0],
+    );
+    setSelectOptionItems(transformData);
+  }, [steps]);
+
+  const handleSelectStep = (option: SelectOption) => {
+    const stepId = option.value;
+    addParamsToUrl({ stepId });
+  };
+
+  return (
+    <Select
+      css={Styled.SelectContainerStyles}
+      selectedOption={selectedOption}
+      onChangeOption={(option) => setSelectedOption(option)}
+      callbackFunction={handleSelectStep}
+      options={selectOptionItems}
+    />
+  );
+};
+
+export default StepSelect;
+
+const defaultValue = { label: '', value: '' };
