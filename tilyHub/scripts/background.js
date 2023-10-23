0ee506dc0ee69d122d4b52b73947e1ac0371cc9e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 06:25:10 +0900

    feat: 유저 온보딩 페이지로 이동

--- tilyHub/scripts/background.js
+++ tilyHub/scripts/background.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 function handleMessage(request, sender, sendResponse) {
-  console.log(request);
   if (request && request.closeWebPage === true && request.isSuccess === true) {
     /* Set username */
     chrome.storage.local.set({ TILyHub_username: request.username });
@@ -11,6 +10,9 @@ function handleMessage(request, sender, sendResponse) {
     /* Close pipe */
     chrome.storage.local.set({ pipe_TILyHub: false });
 
+    /* Go to onboarding for UX */
+    const urlOnboarding = `chrome-extension://${chrome.runtime.id}/welcome.html`;
+    chrome.tabs.create({ url: urlOnboarding, selected: true }); // creates new tab
   } else if (request && request.closeWebPage === true && request.isSuccess === false) {
     alert('Something went wrong while trying to authenticate your profile!');
     chrome.tabs.getSelected(null, function (tab) {
