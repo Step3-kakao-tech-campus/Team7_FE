29775ac0dc2c4202a42e5afa7785eab60ef4789c
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 06:24:56 +0900

    feat: storae.local 에 user정보, 토큰 저장

---
+++ tilyHub/scripts/background.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+function handleMessage(request, sender, sendResponse) {
+  console.log(request);
+  if (request && request.closeWebPage === true && request.isSuccess === true) {
+    /* Set username */
+    chrome.storage.local.set({ TILyHub_username: request.username });
+
+    /* Set token */
+    chrome.storage.local.set({ TILyHub_token: request.token });
+
+    /* Close pipe */
+    chrome.storage.local.set({ pipe_TILyHub: false });
+
+  } else if (request && request.closeWebPage === true && request.isSuccess === false) {
+    alert('Something went wrong while trying to authenticate your profile!');
+    chrome.tabs.getSelected(null, function (tab) {
+      chrome.tabs.remove(tab.id);
+    });
+  }
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
