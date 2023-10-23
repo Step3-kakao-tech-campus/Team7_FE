322e190e087a3bb365a11e532abc380d97cbb305
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 07:52:31 +0900

    feat: commit 모드인지, 레포 선택 단계인지 파악 하는 기능

---
+++ tilyHub/welcome.js
@@ -0,0 +1,51 @@
+
+/* Detect mode type */
+chrome.storage.local.get('mode_type', (data) => {
+  const mode = data.mode_type;
+
+  if (mode && mode === 'commit') {
+    /* Check if still access to repo */
+    chrome.storage.local.get('TILyHub_token', (data2) => {
+      const token = data2.TILyHub_token;
+
+      // 스토리지에 토큰이 없을때 에러 핸들링
+      if (token === null || token === undefined) {
+        /* Not authorized yet. */
+        $('#error').text(
+          'Authorization error - Grant BaekjoonHub access to your GitHub account to continue (click BaekjoonHub extension on the top right to proceed)',
+        );
+        $('#error').show();
+        $('#success').hide();
+        /* Hide accordingly */
+        document.getElementById('hook_mode').style.display = 'inherit';
+        document.getElementById('commit_mode').style.display = 'none';
+      } else {
+        /* Get access to repo */
+        // 스토리지에 저장해야하는 레포의 데이터가 없을때 에러 핸들링
+        chrome.storage.local.get('TILyHub_hook', (repoName) => {
+          const hook = repoName.TILyHub_hook;
+          if (!hook) {
+            /* Not authorized yet. */
+            $('#error').text(
+              'Improper Authorization error - Grant BaekjoonHub access to your GitHub account to continue (click BaekjoonHub extension on the top right to proceed)',
+            );
+            $('#error').show();
+            $('#success').hide();
+            /* Hide accordingly */
+            document.getElementById('hook_mode').style.display = 'inherit';
+            document.getElementById('commit_mode').style.display = 'none';
+          } else {
+            /* Username exists, at least in storage. Confirm this */
+            linkRepo(token, hook);
+          }
+        });
+      }
+    });
+
+    document.getElementById('hook_mode').style.display = 'none';
+    document.getElementById('commit_mode').style.display = 'inherit';
+  } else {
+    document.getElementById('hook_mode').style.display = 'inherit';
+    document.getElementById('commit_mode').style.display = 'none';
+  }
+});
