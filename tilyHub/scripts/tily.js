78ebc9cdbd8494b5760e243e2df06ef2a31cdce0
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 19:32:51 +0900

    feat: 깃허브에 TIL content를 보내는 함수

--- tilyHub/scripts/tily.js
+++ tilyHub/scripts/tily.js
@@ -1,6 +1,17 @@
 // 웹 사이트에서 확장 프로그램이 설치되어있는 확인하기 위함.
 document.documentElement.setAttribute('myextension', true);
 
-document.addEventListener('이벤트', (e) => {
-  console.log(e.detail.content);
+document.addEventListener('크롬익스텐션이벤트', (e) => {
+  commitContentToGitHub({
+    isPersonal: e.detail.isPersonal,
+    roadmapTitle: e.detail.roadmapTitle,
+    stepTitle: e.detail.stepTitle,
+    content: e.detail.content,
+  });
 });
+
+const commitContentToGitHub = async ({ isPersonal, roadmapTitle, stepTitle, content }) => {
+  const updateData = getGitUploadData({ isPersonal, roadmapTitle, stepTitle, content });
+  await beginUpload(updateData);
+};
+
