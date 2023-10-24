90deb6270b8ae9a88a66506c8b90ef996d068052
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 19:35:46 +0900

    feat: Github api를 사용하는 함수

--- tilyHub/scripts/uploadfunctions.js
+++ tilyHub/scripts/uploadfunctions.js
@@ -1,3 +1,4 @@
+/** 업로드 함수
  * 파라미터는 아래와 같습니다.
  * @param {string} readme - 업로드하는 README 내용
  * @param {string} directory - 적용될 폴더 구조
@@ -13,4 +14,27 @@ const uploadOnGitHub = async (updateData, cb) => {
     return;
   }
   return upload(token, hook, updateData.readme, updateData.directory, updateData.message, cb);
+};
+
+/** Github api를 사용하여 업로드를 합니다.
+ * @param {string} token - github api 토큰
+ * @param {string} hook - github api hook
+ * @param {string} sourceText - 업로드할 소스코드
+ * @param {string} readmeText - 업로드할 readme
+ * @param {string} directory - 업로드할 파일의 경로
+ * @param {string} filename - 업로드할 파일명
+ * @param {string} commitMessage - 커밋 메시지
+ * @param {function} cb - 콜백 함수 (ex. 업로드 후 로딩 아이콘 처리 등)
+ */
+const upload = async (token, hook, readmeText, directory, commitMessage, cb) => {
+  /* 업로드 후 커밋 */
+  const git = new GitHub(hook, token);
+  const { refSHA, ref } = await git.getReference();
+  const readme = await git.createBlob(readmeText, `${directory}/README.md`); // readme 파일
+  const treeSHA = await git.createTree(refSHA, [readme]);
+  const commitSHA = await git.createCommit(commitMessage, treeSHA, refSHA);
+  await git.updateHead(ref, commitSHA);
+
+  // 콜백 함수 실행
+  if (typeof cb === 'function') cb();
 };
