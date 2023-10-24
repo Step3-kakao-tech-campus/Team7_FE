45befde3ba9e2ffefd93e53cb387564e24c96917
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-24 19:36:53 +0900

    feat: 크롬 익스텐션의 Local StorageArea 데이터를 가져오는 함수

---
+++ tilyHub/scripts/storage.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+/**
+ * @author https://gist.github.com/sumitpore/47439fcd86696a71bf083ede8bbd5466
+ * Chrome의 Local StorageArea에서 개체 가져오기
+ * @param {string} key
+ */
+const getObjectFromLocalStorage = async (key) => {
+  console.log('getObjectFromLocalStorage');
+
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.local.get(key, function (value) {
+        resolve(value[key]);
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+};
+
+const getHook = async () => {
+  return await getObjectFromLocalStorage('TILyHub_hook');
+};
+
+const getToken = async () => {
+  return await getObjectFromLocalStorage('TILyHub_token');
+};
