af2895f757800919d58ffdce1c2589d16f043180
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-26 02:59:03 +0900

    feat: 새로고침시 스크롤 최상위로 이동

--- src/components/main/Guest/index.tsx
+++ src/components/main/Guest/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import Button from '@/components/common/Button';
@@ -10,6 +11,10 @@ import * as Styled from './style';
 
 const Guest = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
