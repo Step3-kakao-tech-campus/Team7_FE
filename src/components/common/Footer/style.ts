0a1baa0dd1cf117b04b49d41b9974823d316b9f5
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-26 02:25:54 +0900

    feat: 푸터 구현

---
+++ src/components/common/Footer/style.ts
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 4.5rem;
+  margin-top: 8rem;
+  border-top: 1px solid #d2d3d4;
+`;
+
+export const Inner = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  padding: 0 2rem;
+  text-align: left;
+  color: hsl(240 3.8% 46.1%);
+
+  & > a {
+    text-decoration-line: underline;
+    font-weight: 500;
+  }
+`;
