1ae41e6fb9898e96a97048c49a3997c5cabd3c16
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-10-26 17:39:07 +0900

    feat: Responsive 컴포넌트

---
+++ src/components/common/Responsive/index.tsx
@@ -0,0 +1,50 @@
+import { type PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { ResponsiveContext } from '@/components/common/Responsive/provider';
+import { emotionTheme } from '@/styles/emotion';
+
+interface ResponsiveProps {
+  className?: string;
+  device: Device;
+}
+
+type Device = 'mobile' | 'desktop';
+
+const Responsive = (props: PropsWithChildren<ResponsiveProps>) => {
+  const { className = '', children, device } = props;
+
+  const { mobileOnlyClassName, desktopOnlyClassName } = useContext(ResponsiveContext);
+  const [current, setCurrent] = useState<Device | null>(null);
+
+  const { mediaQuery } = emotionTheme;
+
+  const selectedClassName = (() => {
+    if (device === 'desktop') return desktopOnlyClassName;
+    if (device === 'mobile') return mobileOnlyClassName;
+  })();
+
+  useEffect(() => {
+    const mobileMedia = window.matchMedia(mediaQuery.MOBILE_LARGE);
+
+    if (mobileMedia.matches) {
+      setCurrent('mobile');
+    } else {
+      setCurrent('desktop');
+    }
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setCurrent(e.matches ? 'mobile' : 'desktop');
+    };
+
+    mobileMedia.addEventListener('change', handleChange);
+
+    return () => mobileMedia.removeEventListener('change', handleChange);
+  }, []);
+
+  return current === null || device === current ? (
+    <div className={`${selectedClassName} ${className}`}>{children}</div>
+  ) : (
+    <></>
+  );
+};
+
+export default Responsive;
