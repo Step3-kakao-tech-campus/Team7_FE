2d2bade516dbc7d1ea39032179e1026a9d19d535
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-11-06 18:13:55 +0900

    feat: 로그인일 경우 auth 페이지 접근 못하도록 수정

--- src/pages/auth/change-password/index.tsx
+++ src/pages/auth/change-password/index.tsx
@@ -1,3 +1,5 @@
+import type { GetServerSideProps } from 'next';
+import { axiosInstance } from '@/api';
 import PasswordForm from '@/components/auth/change-password/passwordForm';
 import FullHeightLayout from '@/components/layout/FullHeightLayout';
 import { AuthPageContainer } from '@/pages/auth/register/verify';
@@ -14,3 +16,26 @@ const ChangePasswordPage = () => {
 setLayout(ChangePasswordPage, FullHeightLayout);
 
 export default ChangePasswordPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { cookies } = context.req;
+  let isUserLogin = true;
+
+  try {
+    axiosInstance.defaults.headers.common['Authorization'] = cookies['accessToken'];
+    await axiosInstance.get('users');
+  } catch (err) {
+    isUserLogin = false;
+  }
+
+  if (!isUserLogin) {
+    return { props: {} };
+  }
+
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+  };
+};
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,3 +1,5 @@
+import type { GetServerSideProps } from 'next';
+import { axiosInstance } from '@/api';
 import RegisterForm from '@/components/auth/register/RegisterForm';
 import FullHeightLayout from '@/components/layout/FullHeightLayout';
 import { AuthPageContainer } from '@/pages/auth/register/verify';
@@ -14,3 +16,26 @@ export const RegisterPage = () => {
 setLayout(RegisterPage, FullHeightLayout);
 
 export default RegisterPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { cookies } = context.req;
+  let isUserLogin = true;
+
+  try {
+    axiosInstance.defaults.headers.common['Authorization'] = cookies['accessToken'];
+    await axiosInstance.get('users');
+  } catch (err) {
+    isUserLogin = false;
+  }
+
+  if (!isUserLogin) {
+    return { props: {} };
+  }
+
+  return {
+    redirect: {
+      destination: '/',
+      permanent: false,
+    },
+  };
+};
