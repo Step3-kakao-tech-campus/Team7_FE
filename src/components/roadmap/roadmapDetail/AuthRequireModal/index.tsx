a9db32578770dcc304b2a697813ffab94e1e99ed
Junseo <monstazo911@gmail.com>
2023-11-12 18:48:53 +0900

    fix: 틸리 카드 제목

--- src/components/roadmap/roadmapDetail/AuthRequireModal/index.tsx
+++ src/components/roadmap/roadmapDetail/AuthRequireModal/index.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import AuthLogo from '@/components/auth/common/AuthLogo';
 import Button from '@/components/common/Button';
@@ -14,7 +13,6 @@ const AuthRequireModal = (props: ModalProps) => {
       <Flex dir="col" align="center" gap={1.5}>
         <AuthLogo />
         <h3>로그인 후 다양한 로드맵들을 만나보세요!</h3>
-        <Image src="/assets/icons/ic_welcome.svg" width={200} height={200} alt="환영 이미지" />
         <Button onClick={() => router.push(TILY_LINKS.login())} fullWidth>
           시작하기
         </Button>
